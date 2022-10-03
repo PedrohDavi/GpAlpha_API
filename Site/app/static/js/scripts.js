0d3ce7ef9173c814a06c289361511909ae6b73ae
guscomparotto <gcomparottom@gmail.com>
2022-10-03 12:21:49 -0300

    up

--- Site/app/static/js/scripts.js
+++ Site/app/static/js/scripts.js
@@ -1,3 +1,8 @@
+/* STEPS */
+var form_2_progessbar = document.querySelector(".form_2_progessbar");
+var form_3_progessbar = document.querySelector(".form_3_progessbar");
+var form_4_progessbar = document.querySelector(".form_4_progessbar");
+
 /* DIV CONTENTS */
 var form_1 = document.querySelector(".form_1");
 var form_2 = document.querySelector(".form_2");
@@ -17,20 +22,81 @@ var form_2_next_btn = document.querySelector(".form_2_btns .btn_next");
 var form_3_back_btn = document.querySelector(".form_3_btns .btn_back");
 var form_3_next_btn = document.querySelector(".form_3_btns .btn_next");
 
-/* STEPS */
-var form_2_progessbar = document.querySelector(".form_2_progessbar");
-var form_3_progessbar = document.querySelector(".form_3_progessbar");
-var form_4_progessbar = document.querySelector(".form_4_progessbar");
+const sizeRooms = {
+	larger: [402], /* 4x4 - 4 */
+	medium: [301, 302, 401], /* 3x3 - 4 */
+	small: [303, 404, 405, 406, 407, 408, 409, 411, 412] /* 3x3 - 3 */
+}
+
+const lines = ['firstColumn','tenthColumn','secondColumn','ninthColumn','fifthRow'];
+
+function loadLayout(target) { /* FUNÇÃO QUE MODIFICA DINAMICAMENTE O LAYOUT DOS COMPUTADORES */
+	for(var currentSize in sizeRooms){ /* FAZ UM LOOP PELOS TAMANHOS DE SALA */
+		sizeRooms[currentSize].forEach(room => { /* FAZ UM LOOP A PROCURA DA SALA */
+			if(room === target){ /* SE ACHOU A SALA */
+				var limit = 0;
+				currentSize === 'small' ? limit = 5 :
+				currentSize === 'medium' ? limit = 4 : limit = 2;			
+				for(var i = 0; i < limit; i++){
+					[].forEach.call(document.querySelectorAll('.'+lines[i]), function (el) {
+						el.style.visibility = 'hidden';						
+					});
+				};
+			};
+		});
+	};
+};
+
+var floors = {
+	floor3: [301,302, 303],
+	floor4: [401, 402, 404, 405, 406, 407, 408, 409, 411, 412]
+}
+
+/* CÓDIGO SELEÇÃO DE SALAS */
+function selectOptionCreate() {	
+
+	var val = document.getElementById("c1_Floor").value;
+	var select = document.getElementById('datalistOptionsRoom');
+	
+	/* var target = floor3;
+	if (val === '3º Andar') {
+		target = 0
+	} else {
+		target = 1
+	}; */
+
+	console.log(floors[0]);
+
+	floors[floor3].forEach(r => {
+		var opt = document.createElement('option');
+		opt.value = r;
+		opt.innerHTML = r;
+		select.appendChild(opt);
+	});
+		
+}
 
 form_1_next_btn.addEventListener("click", function(){
 
-	const name = document.getElementById("c1_name").value;
+	/* const name = document.getElementById("c1_name").value;
 	const email = document.getElementById("c1_email").value;
 
 	if (name === '' || email === '') {
 		alert("ERRO: Você não preencheu os dados necessários!");
-		return; /* BLOQUEIA O RESTO DO SCRIPT */
-	}
+		return;  ## BLOQUEIA O RESTO DO SCRIPT ##
+	} */
+
+	/* 
+	4x4 - 4 ##### 402 
+	3x3 - 4 ##### 301, 302, 401
+	3x3 - 3 ##### 303, 404, 405, 406, 407, 408, 409, 411, 412
+	*/
+
+	/* const target = 402;	
+
+	loadLayout(target);
+
+	selectOptionCreate(target); */
 
 	form_1.style.display = "none";
 	form_2.style.display = "block";
@@ -53,21 +119,21 @@ form_2_back_btn.addEventListener("click", function(){
 
 form_2_next_btn.addEventListener("click", function(){
 
-	var pcChecked = false;
-	const pcs = document.getElementsByName("c2_computadores"); /* CRIA UM DICIONÁRIO COM TODOS OS ELEMENTOS NO HTML QUE TEM O ATRIBUTO name="c2_computadores" */
+	/* var pcChecked = false;
+	const pcs = document.getElementsByName("c2_computadores"); ## CRIA UM DICIONÁRIO COM TODOS OS ELEMENTOS NO HTML QUE TEM O ATRIBUTO name="c2_computadores" ##
 
-	/* FAZ UM LOOP PRA VER SE TEM UM COMPUTADOR SELECIONADO */
+	## FAZ UM LOOP PRA VER SE TEM UM COMPUTADOR SELECIONADO ##
 	pcs.forEach(pc => {
 		if (pc.checked === true) {
 			pcChecked = true;
-			return; /* SAI DO LOOP */
+			return; ## SAI DO LOOP ##
 		}
 	});
 
 	if (pcChecked === false) {
 		alert("ERRO: Você não escolheu o computador!");
-		return; /* BLOQUEIA O RESTO DO SCRIPT */
-	}
+		return; ## BLOQUEIA O RESTO DO SCRIPT ##
+	} */
 
 	form_2.style.display = "none";
 	form_3.style.display = "block";
@@ -90,13 +156,13 @@ form_3_back_btn.addEventListener("click", function(){
 
 form_3_next_btn.addEventListener("click", function(){
 
-	const subject = document.getElementById("c3_assunto").value;
+	/* const subject = document.getElementById("c3_assunto").value;
 	const description = document.getElementById("c3_texto").value;
 	
 	if (subject === '' || description === '') {
 		alert("ERRO: Você não preencheu os dados necessários!");
-		return; /* BLOQUEIA O RESTO DO SCRIPT */
-	}
+		return; ## BLOQUEIA O RESTO DO SCRIPT ##
+	} */
 
 	form_3.style.display = "none";
 	form_4.style.display = "block";
